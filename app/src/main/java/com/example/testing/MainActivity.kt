1318ab057ab947363534ebec1d9e0fd505b59392
Vikas Suthar <prsuthar44@gmail.com>
2022-12-25 10:53:13 +0530

    Update MainActivity.kt
    third commit by vikas

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 //first commit by vikas suthar
 //second commit by vikas suthar
+//third commit by vikas suthar
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
